a583cb1b380a42839c239337472a31c1a58f1d8b
Mainul islam <mainulislam163@gmail.com>
2021-10-23 23:46:16 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.scss";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import MovieDetail from "./Components/MovieDetail/MovieDetail";
+import PageNotFound from "./Components/PageNotFound/PageNotFound";
 function App() {
-  return <div>app</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:movieId" component={MovieDetail} />
+          <Route path="/pageNotFound" component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
