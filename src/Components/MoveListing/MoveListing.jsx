356b98848a16f0e74163ad12f01d07ffb5ed6bcb
Mainul islam <mainulisraf163@gmail.com>
2022-01-16 23:51:40 +0600

    antd grid not working

--- src/Components/MoveListing/MoveListing.jsx
+++ src/Components/MoveListing/MoveListing.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-import { Result } from "antd";
+import { Row, Col } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllSeriesInfo } from "../../store/seriesList/SeriesListAction";
 import MovieCard from "./../MovieCard/MovieCard";
+import "./MoveListing.scss";
 
 const MoveListing = () => {
   const dispatch = useDispatch();
@@ -14,9 +15,48 @@ const MoveListing = () => {
     seriesdataList();
   }, []);
   console.log(movieList.seriesList.seriesListData);
+  let renderMovies = "";
+  renderMovies =
+    movieList.seriesList.seriesListData.Response === "True" ? (
+      <Row>
+        {movieList.seriesList.seriesListData.Search.map((data, index) => (
+          <MovieCard movie={data} key={index + 1} />
+        ))}
+      </Row>
+    ) : (
+      // movieList.seriesList.seriesListData.Search.map((data, index) => (
+      //   <Row justify="space-between">
+      //     <Col xxl={5} xl={5}>
+      //       <MovieCard movie={data} key={index + 1} />
+      //     </Col>
+      //   </Row>
+      // ))
+      <div className="movies-erros">
+        <h1>{movieList.seriesList.seriesListData.Error}</h1>
+      </div>
+    );
   return (
     <>
-      <MovieCard />
+      <Row justify="end">
+        <Col xxl={12} xl={12} md={4} lg={4} style={{ border: "1px solid red" }}>
+          <p>Movies</p>
+        </Col>
+        <Col xxl={12} xl={12} md={2} style={{ border: "1px solid red" }}>
+          <p>Movies</p>
+        </Col>
+        <Col xxl={6} xl={6} md={2} style={{ border: "1px solid red" }}>
+          <h2>Movies</h2>
+        </Col>
+        <Col xxl={6} xl={6} md={2} style={{ border: "1px solid red" }}>
+          <h2>Movies</h2>
+        </Col>
+      </Row>
+      <div className="movie-wrapper">
+        <div className="movie-list">
+          <h2>Movies</h2>
+        </div>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
     </>
   );
 };
--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
-const MovieCard = () => {
-  return <div>Ifsdfgdsdgg</div>;
+import { Card, Col } from "antd";
+const { Meta } = Card;
+const MovieCard = ({ movie }) => {
+  console.log(movie);
+  return (
+    <Col xxl={6} xl={6} md={2}>
+      <Card hoverable cover={<img alt="example" src={movie.Poster} />}>
+        {" "}
+        <Meta title="Europe Street beat" description="www.instagram.com" />
+      </Card>
+    </Col>
+  );
 };
 
 export default MovieCard;
